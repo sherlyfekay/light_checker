03ddeb0839419fc4c3ea966765e818e1c6f3df53
sherlyfekay <sherlyfekay@gmail.com>
2017-06-19 13:16:38 +0700

    Database done

--- app/src/main/java/com/example/sherly/lightchecker/MainActivity.java
+++ app/src/main/java/com/example/sherly/lightchecker/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sherly.lightchecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
